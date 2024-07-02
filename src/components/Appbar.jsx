ac7e95caf94f6356ba026cab16c07bed8bb0f63d
GAURAV SHARMA <108004723+gauravrdj@users.noreply.github.com>
2024-07-02 20:42:51 +0530

    suspended

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -4,7 +4,7 @@ export const Appbar = () => {
     const navigate=useNavigate();
     return <div className="shadow h-14 flex justify-between">
         <div className="flex flex-col justify-center h-full ml-4 text-blue-700">
-            PayTM App
+            Payments App
         </div>
         <div className="flex">
             <div className="flex flex-col justify-center h-full mr-4 text-blue-700">
